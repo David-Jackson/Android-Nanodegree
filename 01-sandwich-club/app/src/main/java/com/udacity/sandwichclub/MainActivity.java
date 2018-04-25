fd045135e5335e705b3525423b10f6ce284c69ae
David Jackson <jack8679@kettering.edu>
2018-04-25 13:48:44 -0400

    Added BottomSheet to Main Activity

--- 01-sandwich-club/app/src/main/java/com/udacity/sandwichclub/MainActivity.java
+++ 01-sandwich-club/app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -3,15 +3,21 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
+
+import com.udacity.sandwichclub.handlers.BottomSheetHandler;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    Switch detailSwitch;
+    BottomSheetHandler bottomSheetHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +28,28 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, sandwiches);
 
+        detailSwitch = findViewById(R.id.switch_detail);
+        detailSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                buttonView.setText(isChecked ?
+                        R.string.detail_switch_on_text : R.string.detail_switch_off_text);
+            }
+        });
+
+        bottomSheetHandler = new BottomSheetHandler(this);
+
         // Simplification: Using a ListView instead of a RecyclerView
         ListView listView = findViewById(R.id.sandwiches_listview);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Log.d(TAG, "onItemClick: clicked position: " + position);
-                launchDetailActivity(position);
+                if (detailSwitch.isChecked()) {
+                    bottomSheetHandler.openBottomSheet(position);
+                } else {
+                    launchDetailActivity(position);
+                }
             }
         });
     }
---
+++ 01-sandwich-club/app/src/main/java/com/udacity/sandwichclub/handlers/BottomSheetHandler.java
@@ -0,0 +1,71 @@
+package com.udacity.sandwichclub.handlers;
+
+import android.app.Activity;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.BottomSheetBehavior;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.udacity.sandwichclub.R;
+
+// This class handles all of the UI for the sandwich detail view as a BottomSheet
+public class BottomSheetHandler extends DetailViewHandler {
+
+    View scrim;
+    ConstraintLayout bottomSheetViewgroup;
+    BottomSheetBehavior bottomSheetBehavior;
+
+    public BottomSheetHandler(Activity activity) {
+        super(activity);
+
+        scrim = activity.findViewById(R.id.scrim);
+        scrim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // when the scrim is clicked, hide the scrim and bottom sheet
+                scrim.setVisibility(View.INVISIBLE);
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            }
+        });
+
+        bottomSheetViewgroup = activity.findViewById(R.id.bottom_sheet);
+        bottomSheetViewgroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // set onclick so the touch event is not passed to the scrim
+            }
+        });
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetViewgroup);
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                    scrim.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+        bottomSheetBehavior.setPeekHeight(
+                (int) activity.getResources().getDimension(R.dimen.sandwich_image_height));
+        bottomSheetBehavior.setHideable(true);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+    }
+
+    public void openBottomSheet(int position) {
+        if (loadSandwich(position)) {
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            scrim.setVisibility(View.VISIBLE);
+        } else {
+            Toast.makeText(activity, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
