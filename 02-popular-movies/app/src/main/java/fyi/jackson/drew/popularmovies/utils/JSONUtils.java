cec7ba986e873dd8bdc7feff0cdb8cf381e90add
David Jackson <jack8679@kettering.edu>
2018-04-26 10:32:38 -0400

    Added JSONUtils to parse Movie and Genre JSON

---
+++ 02-popular-movies/app/src/main/java/fyi/jackson/drew/popularmovies/utils/JSONUtils.java
@@ -0,0 +1,76 @@
+package fyi.jackson.drew.popularmovies.utils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fyi.jackson.drew.popularmovies.model.Genre;
+import fyi.jackson.drew.popularmovies.model.Movie;
+
+public class JSONUtils {
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_OVERVIEW = "overview";
+    public static final String KEY_TAGLINE = "tagline";
+    public static final String KEY_POSTER_PATH = "poster_path";
+    public static final String KEY_BACKDROP_PATH = "backdrop_path";
+    public static final String KEY_BUDGET = "budget";
+    public static final String KEY_REVENUE = "revenue";
+    public static final String KEY_POPULARITY = "popularity";
+    public static final String KEY_VIDEO = "video";
+    public static final String KEY_VOTE_AVERAGE = "vote_average";
+    public static final String KEY_VOTE_COUNT = "vote_count";
+    public static final String KEY_GENRES = "GENRES";
+    public static final String KEY_RELEASE_DATE = "release_date";
+
+    public static final String KEY_NAME = "name";
+
+    public static Movie parseMovie(String jsonString) throws JSONException {
+        JSONObject json = new JSONObject(jsonString);
+        return parseMovie(json);
+    }
+
+    public static Movie parseMovie(JSONObject json) {
+        Movie movie = new Movie();
+        
+        movie.setId(json.optInt(KEY_ID));
+        movie.setTitle(json.optString(KEY_TITLE));
+        movie.setOverview(json.optString(KEY_OVERVIEW));
+        movie.setTagline(json.optString(KEY_TAGLINE));
+        movie.setPosterPath(json.optString(KEY_POSTER_PATH));
+        movie.setBackdropPath(json.optString(KEY_BACKDROP_PATH));
+        movie.setBudget(json.optInt(KEY_BUDGET));
+        movie.setRevenue(json.optInt(KEY_REVENUE));
+        movie.setPopularity(json.optDouble(KEY_POPULARITY));
+        movie.setVideo(json.optBoolean(KEY_VIDEO));
+        movie.setVoteAverage(json.optDouble(KEY_VOTE_AVERAGE));
+        movie.setVoteCount(json.optInt(KEY_VOTE_COUNT));
+        movie.setGenres(jsonToGenresList(json.optJSONArray(KEY_GENRES)));
+        movie.setReleaseDate(json.optString(KEY_RELEASE_DATE));
+
+        return movie;
+    }
+
+    public static Genre parseGenre(JSONObject json) {
+        return new Genre(
+                json.optInt(KEY_ID),
+                json.optString(KEY_NAME)
+        );
+    }
+
+    private static List<Genre> jsonToGenresList(JSONArray json) {
+        List<Genre> genres = new ArrayList<>();
+        if (json != null) {
+            for (int i = 0; i < json.length(); i++) {
+                JSONObject genreJson = json.optJSONObject(i);
+                if (genreJson == null) continue;
+                genres.add(parseGenre(genreJson));
+            }
+        }
+        return genres;
+    }
+}
