fada39ab4b75b60de769a97feeb9d8bf5fcabe9f
David Jackson <jack8679@kettering.edu>
2018-04-26 10:32:03 -0400

    Added Model classes for Movies and Genres

---
+++ 02-popular-movies/app/src/main/java/fyi/jackson/drew/popularmovies/model/Movie.java
@@ -0,0 +1,148 @@
+package fyi.jackson.drew.popularmovies.model;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class Movie {
+
+    int id;
+    String title;
+    String overview;
+    String tagline;
+    String posterPath;
+    String backdropPath;
+    int budget;
+    int revenue;
+    Double popularity;
+    boolean video;
+    Double voteAverage;
+    int voteCount;
+    List<Genre> genres;
+    String releaseDate;
+
+    // optional attributes
+//    String originalTitle;
+//    String imdbId;
+//    Integer collection;
+//    boolean adult;
+//    String homepage;
+//    String originalLanguage;
+//    String status;
+
+    public Movie() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getTagline() {
+        return tagline;
+    }
+
+    public void setTagline(String tagline) {
+        this.tagline = tagline;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+    public int getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(int revenue) {
+        this.revenue = revenue;
+    }
+
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+    }
+
+    public boolean isVideo() {
+        return video;
+    }
+
+    public void setVideo(boolean video) {
+        this.video = video;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
