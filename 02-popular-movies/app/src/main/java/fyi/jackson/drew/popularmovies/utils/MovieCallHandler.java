5731ae7cdc88411c5a25c348d5c1c64a87eebc07
David Jackson <jack8679@kettering.edu>
2018-05-01 10:39:44 -0400

    Added MovieCallHandler to handle and cache Retrofit calls

---
+++ 02-popular-movies/app/src/main/java/fyi/jackson/drew/popularmovies/utils/MovieCallHandler.java
@@ -0,0 +1,53 @@
+package fyi.jackson.drew.popularmovies.utils;
+
+import android.util.Log;
+
+import fyi.jackson.drew.popularmovies.model.MovieList;
+import fyi.jackson.drew.popularmovies.recycler.MovieListAdapter;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MovieCallHandler {
+
+    private static final String TAG = MovieCallHandler.class.getSimpleName();
+
+    private int attempts = 0;
+    Call<MovieList> call;
+    MovieListAdapter adapter;
+    MovieList movieList;
+
+    public MovieCallHandler(Call<MovieList> call, MovieListAdapter adapter) {
+        this.call = call;
+        this.adapter = adapter;
+    }
+
+    public void populateAdapter() {
+        if (movieList == null) {
+            if (attempts++ > 10) {
+                Log.e(TAG, "populateAdapter: Attempts exceeded limit");
+                return;
+            }
+            executeCall();
+        } else {
+            adapter.setMovieList(movieList.getResults());
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    private void executeCall() {
+        call.enqueue(new Callback<MovieList>() {
+            @Override
+            public void onResponse(Call<MovieList> call, Response<MovieList> response) {
+                movieList = response.body();
+                populateAdapter();
+            }
+
+            @Override
+            public void onFailure(Call<MovieList> call, Throwable t) {
+                Log.e(TAG, "onFailure: Call Failed to execute");
+            }
+        });
+    }
+
+}
