e8b260a65ab6ef40e3aef81e58e573ef4549eb52
David Jackson <jack8679@kettering.edu>
2018-05-20 13:34:06 -0400

    Added placeholder images

---
+++ 04-baking-app/app/src/main/java/fyi/jackson/drew/rezept/util/RecipeUtils.java
@@ -0,0 +1,22 @@
+package fyi.jackson.drew.rezept.util;
+
+import fyi.jackson.drew.rezept.R;
+
+public class RecipeUtils {
+
+    private static int[] placeholderImages = {
+            R.mipmap.img_bbq_2x,
+            R.mipmap.img_breakfast_2x,
+            R.mipmap.img_lunch_2x,
+            R.mipmap.img_dinner_2x
+    };
+
+    private static int placeHolderImageIndex = 0;
+
+    public static int getPlaceHolderImage() {
+        placeHolderImageIndex++;
+        placeHolderImageIndex %= placeholderImages.length;
+        return placeholderImages[placeHolderImageIndex];
+    }
+
+}
