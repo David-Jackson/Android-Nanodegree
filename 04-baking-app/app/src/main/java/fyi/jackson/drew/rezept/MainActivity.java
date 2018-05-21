b5fa80ff7f07d33f69ef4ce72c51af5e1598c4f5
David Jackson <jack8679@kettering.edu>
2018-05-21 12:19:16 -0400

    Added layout for tablets

--- 04-baking-app/app/src/main/java/fyi/jackson/drew/rezept/MainActivity.java
+++ 04-baking-app/app/src/main/java/fyi/jackson/drew/rezept/MainActivity.java
@@ -2,8 +2,10 @@ package fyi.jackson.drew.rezept;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-import fyi.jackson.drew.rezept.fragment.RecipeListFragment;
+import fyi.jackson.drew.rezept.fragment.DetailFragment;
+import fyi.jackson.drew.rezept.fragment.ListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.content, RecipeListFragment.newInstance())
+                .add(R.id.content, ListFragment.newInstance())
                 .commit();
+
+        View detailView = findViewById(R.id.detail);
+        boolean isTablet = detailView != null;
+
+        if (isTablet) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.detail, DetailFragment.newInstance())
+                    .commit();
+        }
     }
 }
