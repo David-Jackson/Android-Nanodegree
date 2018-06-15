a476a63d989725684aabdeec81b93649ac58bc78
David Jackson <davidjackson548@gmail.com>
2018-06-15 17:16:06 -0400

    Added DisplayHome back arrow for detail fragment

--- 04-baking-app/app/src/main/java/fyi/jackson/drew/rezept/MainActivity.java
+++ 04-baking-app/app/src/main/java/fyi/jackson/drew/rezept/MainActivity.java
@@ -1,13 +1,15 @@
 package fyi.jackson.drew.rezept;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.view.View;
 
 import fyi.jackson.drew.rezept.fragment.DetailFragment;
 import fyi.jackson.drew.rezept.fragment.ListFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements FragmentManager.OnBackStackChangedListener {
 
     boolean isTablet;
 
@@ -23,6 +25,28 @@ public class MainActivity extends AppCompatActivity {
 
         View detailView = findViewById(R.id.detail);
         isTablet = detailView != null;
+
+        getSupportFragmentManager().addOnBackStackChangedListener(this);
+        //Handle when activity is recreated like on orientation Change
+        shouldDisplayHomeUp();
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        shouldDisplayHomeUp();
+    }
+
+    public void shouldDisplayHomeUp(){
+        //Enable Up button only  if there are entries in the back stack
+        boolean canback = getSupportFragmentManager().getBackStackEntryCount()>0;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(canback);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        //This method is called when the up button is pressed. Just the pop back stack.
+        getSupportFragmentManager().popBackStack();
+        return true;
     }
 
     public boolean isTablet() {
