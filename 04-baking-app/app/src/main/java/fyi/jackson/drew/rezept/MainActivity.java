234f2858f3b474c84e7f2a077a41af9c292e96c3
David Jackson <jack8679@kettering.edu>
2018-06-20 16:55:23 -0400

    Implemented AVD for AppBar icon

--- 04-baking-app/app/src/main/java/fyi/jackson/drew/rezept/MainActivity.java
+++ 04-baking-app/app/src/main/java/fyi/jackson/drew/rezept/MainActivity.java
@@ -1,17 +1,21 @@
 package fyi.jackson.drew.rezept;
 
-import android.support.v7.app.AppCompatActivity;
-import android.support.v4.app.FragmentManager;
+import android.annotation.SuppressLint;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import fyi.jackson.drew.rezept.fragment.DetailFragment;
 import fyi.jackson.drew.rezept.fragment.ListFragment;
 
 public class MainActivity extends AppCompatActivity
         implements FragmentManager.OnBackStackChangedListener {
 
     boolean isTablet;
+    AnimatedVectorDrawable iconAvd, backAvd;
+    boolean wasInDetailFragment = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,11 @@ public class MainActivity extends AppCompatActivity
         View detailView = findViewById(R.id.detail);
         isTablet = detailView != null;
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            iconAvd = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_dining_to_back);
+            backAvd = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_back_to_dining);
+        }
+
         getSupportFragmentManager().addOnBackStackChangedListener(this);
         //Handle when activity is recreated like on orientation Change
         shouldDisplayHomeUp();
@@ -36,10 +45,28 @@ public class MainActivity extends AppCompatActivity
         shouldDisplayHomeUp();
     }
 
+    @SuppressLint("NewApi")
     public void shouldDisplayHomeUp(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //Enable Up button only  if there are entries in the back stack
-        boolean canback = getSupportFragmentManager().getBackStackEntryCount()>0;
-        getSupportActionBar().setDisplayHomeAsUpEnabled(canback);
+        boolean canBack = getSupportFragmentManager().getBackStackEntryCount()>0;
+        boolean canAvd = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+        if (canBack) {
+            if (canAvd && !wasInDetailFragment) {
+                getSupportActionBar().setHomeAsUpIndicator(iconAvd);
+                iconAvd.start();
+            } else {
+                getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
+            }
+        } else {
+            if (canAvd && wasInDetailFragment) {
+                getSupportActionBar().setHomeAsUpIndicator(backAvd);
+                backAvd.start();
+            } else {
+                getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_local_dining_white_24dp);
+            }
+        }
+        wasInDetailFragment = canBack;
     }
 
     @Override
