eeb6a31abdc73ff8c699892f6514c7584975663c
David Jackson <jack8679@kettering.edu>
2018-08-21 12:58:43 -0400

    Added AsyncTask to connect to app engine

--- 05-build-it-bigger/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ 05-build-it-bigger/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,8 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -47,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Mr. Roboto"));
         Intent intent = new Intent(this, JokeViewerActivity.class);
         intent.setAction(JokeViewerActivity.ACTION_DISPLAY_JOKE);
         intent.putExtra(JokeViewerActivity.EXTRA_JOKE, joker.getJoke());
