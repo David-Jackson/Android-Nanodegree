05bedac1f743152b4caf36cd7a213d210b00de5c
David Jackson <jack8679@kettering.edu>
2018-08-27 16:30:51 -0400

    Utilizes new URL for data, with much smaller bodies of text

--- 06-make-your-app-material/XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ 06-make-your-app-material/XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -9,10 +9,18 @@ public class Config {
     public static final URL BASE_URL;
     private static String TAG = Config.class.toString();
 
+    // This is the original URL provided in the project, with MASSIVE bodies of text
+    // that take a while to render
+    private static final String DEFAULT_UDACITY_URL = "https://go.udacity.com/xyz-reader-json";
+
+    // This is a URL for the same data provided in the default above, except the bodies of text
+    // have been trimmed to only 5000 characters
+    private static final String TRIMMED_URL = "https://api.jsonbin.io/b/5b845e403ffac56f4bd59782";
+
     static {
         URL url = null;
         try {
-            url = new URL("https://go.udacity.com/xyz-reader-json" );
+            url = new URL(TRIMMED_URL);
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
             Log.e(TAG, "Please check your internet connection.");
