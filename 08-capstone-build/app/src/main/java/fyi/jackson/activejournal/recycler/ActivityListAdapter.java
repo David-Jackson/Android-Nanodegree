314532b7eac655e646ec35b79e1665cc55a4de7f
David Jackson <jack8679@kettering.edu>
2018-09-10 14:01:43 -0400

    Added spacer to bottom of recycler to avoid bottom sheet

--- 08-capstone-build/app/src/main/java/fyi/jackson/activejournal/recycler/ActivityListAdapter.java
+++ 08-capstone-build/app/src/main/java/fyi/jackson/activejournal/recycler/ActivityListAdapter.java
@@ -13,6 +13,9 @@ import fyi.jackson.activejournal.data.entities.Activity;
 
 public class ActivityListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+    private static final int VIEW_TYPE_ACTIVITY = 289;
+    private static final int VIEW_TYPE_SPACER = 854;
+
     private List<Activity> activities;
 
     public ActivityListAdapter() {
@@ -23,18 +26,36 @@ public class ActivityListAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View v = inflater.inflate(R.layout.view_holder_activity, parent, false);
-        return new ActivityViewHolder(v);
+        View v;
+        RecyclerView.ViewHolder viewHolder;
+        if (viewType == VIEW_TYPE_SPACER) {
+            // Spacer Viewholder
+            v = inflater.inflate(R.layout.view_holder_spacer, parent, false);
+            viewHolder = new SpacerViewHolder(v);
+        } else {
+            v = inflater.inflate(R.layout.view_holder_activity, parent, false);
+            viewHolder = new ActivityViewHolder(v);
+        }
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
-        ((ActivityViewHolder) viewHolder).bindTo(activities.get(position));
+        if (position == activities.size()) {
+            // Spacer Viewholder
+        } else {
+            ((ActivityViewHolder) viewHolder).bindTo(activities.get(position));
+        }
     }
 
     @Override
     public int getItemCount() {
-        return (activities == null ? 0 : activities.size());
+        return (activities == null ? 0 : activities.size() + 1);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (activities.size() == position) ? VIEW_TYPE_SPACER : VIEW_TYPE_ACTIVITY;
     }
 
     public void setActivities(List<Activity> activities) {
---
+++ 08-capstone-build/app/src/main/java/fyi/jackson/activejournal/recycler/SpacerViewHolder.java
@@ -0,0 +1,11 @@
+package fyi.jackson.activejournal.recycler;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class SpacerViewHolder extends RecyclerView.ViewHolder {
+    public SpacerViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
