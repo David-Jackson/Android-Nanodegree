c679a858ee7cce2134d801d72e13b3a01f829999
David Jackson <jack8679@kettering.edu>
2018-09-13 17:02:21 -0400

    Load map thumbnail asynchronously

--- 08-capstone-build/app/src/main/java/fyi/jackson/activejournal/recycler/ActivityViewHolder.java
+++ 08-capstone-build/app/src/main/java/fyi/jackson/activejournal/recycler/ActivityViewHolder.java
@@ -1,11 +1,18 @@
 package fyi.jackson.activejournal.recycler;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 import fyi.jackson.activejournal.R;
 import fyi.jackson.activejournal.data.entities.Activity;
@@ -31,15 +38,15 @@ public class ActivityViewHolder extends RecyclerView.ViewHolder {
         type.setImageResource(activity.getTypeResId());
 
         if (activity.getThumbnail() != null) {
+            File f = new File(
+                    itemView.getContext().getDir("thumbnails", Context.MODE_PRIVATE),
+                    activity.getThumbnail());
 
-            Bitmap bitmap = new ImageSaver(itemView.getContext())
-                    .setExternal(false)
-                    .setDirectoryName("thumbnails")
-                    .setFileName(activity.getThumbnail())
-                    .load();
-
-            map.setImageBitmap(bitmap);
-
+            Picasso.get()
+                    .load(f)
+                    .placeholder(R.drawable.image_activity_map_placeholder)
+                    .error(R.drawable.image_activity_map_placeholder)
+                    .into(map);
         } else {
             map.setImageResource(R.drawable.image_activity_map_placeholder);
         }
